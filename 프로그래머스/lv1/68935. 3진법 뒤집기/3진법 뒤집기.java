05a501653287c10c6afdf67d8c6a96dc9dfb1647
Woohyun Park <53467997+JoshWPark@users.noreply.github.com>
2023-04-08 14:53:04 +0900

    [level 1] Title: 3진법 뒤집기, Time: 8.64 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,42 +1,37 @@
 import java.util.*;
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        String str = Integer.toString(n,3);
+        String reverse = "";
+        for (int i = str.length()-1; i>=0; i--){
+            reverse += str.charAt(i);
+        }
+        //10 진법으로 변경
+        
+        answer = Integer.parseInt(reverse,3);
+        return answer;
+    }
+}
+
+
+// class Solution {
+//     public int solution(int n) {
+//         int answer =0;
+//         String str = "";
+//         while(n>0){
+//             str += n%3;
+//             n/=3;
+//         }
+//         answer = Integer.parseInt(str, 3);
+//         return answer;
+//     }
+// }
+       
 // class Solution {
 //     public int solution(int n) {
 //         int answer = 0;
 //         int triNum = 0; //3진법
-//         int cnt = 0; //제곱근
-//         //3진법 구하기
-//         while (n>0){
-//             triNum = triNum + n%3 * (int)Math.pow(10,cnt);
-//             n /= 3;
-//             cnt++;
-//         }
-//         //앞뒤 반전
-//         int reverse = 0; //앞뒤 반전 값
-//         while(triNum>0){
-//             reverse = reverse * 10 + triNum%10;
-//             triNum/=10;
-//         }
-//         //10 진법으로 변경
-//         String ans = "" + reverse;
-//         answer = Integer.parseInt(ans,3);
-//         return answer;
-//     }
-// }
-class Solution {
-    public int solution(int n) {
-        int answer =0;
-        String str = "";
-        while(n>0){
-            str += n%3;
-            n/=3;
-        }
-        answer = Integer.parseInt(str, 3);
-        return answer;
-    }
-}
-       
-
-// int triNum = 0; //3진법
 //         int cnt = 0; //제곱근
 //         //3진법 구하기
 //         while (n>0){
@@ -57,4 +52,8 @@ class Solution {
         
 //         //10 진법으로 변경
 //         answer = Integer.parseInt(tempAns, 3);
+//         return answer;
+//     }
+// }
+
         
