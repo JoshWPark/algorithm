4fede8a2bc82fb7bc1374a88269d7a1b336c40fd
Woohyun Park <53467997+JoshWPark@users.noreply.github.com>
2023-04-08 14:23:46 +0900

    [level 1] Title: 3진법 뒤집기, Time: 11.07 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,7 +1,30 @@
 import java.util.*;
+// class Solution {
+//     public int solution(int n) {
+//         int answer = 0;
+//         int triNum = 0; //3진법
+//         int cnt = 0; //제곱근
+//         //3진법 구하기
+//         while (n>0){
+//             triNum = triNum + n%3 * (int)Math.pow(10,cnt);
+//             n /= 3;
+//             cnt++;
+//         }
+//         //앞뒤 반전
+//         int reverse = 0; //앞뒤 반전 값
+//         while(triNum>0){
+//             reverse = reverse * 10 + triNum%10;
+//             triNum/=10;
+//         }
+//         //10 진법으로 변경
+//         String ans = "" + reverse;
+//         answer = Integer.parseInt(ans,3);
+//         return answer;
+//     }
+// }
 class Solution {
     public int solution(int n) {
-        int answer = 0;
+        int answer =0;
         String str = "";
         while(n>0){
             str += n%3;
@@ -11,6 +34,7 @@ class Solution {
         return answer;
     }
 }
+       
 
 // int triNum = 0; //3진법
 //         int cnt = 0; //제곱근
