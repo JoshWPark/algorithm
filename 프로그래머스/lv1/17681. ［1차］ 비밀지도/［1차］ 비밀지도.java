f6da7df79003243d7c8cb3874ce69827e8b8c0cf
Woohyun Park <53467997+JoshWPark@users.noreply.github.com>
2023-04-12 11:48:06 +0900

    [level 1] Title: [1차] 비밀지도, Time: 7.24 ms, Memory: 77.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = new String [n];
+        for (int  i = 0; i<n ; i++){
+            answer[i] = String.format("%"+n+"s",Integer.toBinaryString(arr1[i] | arr2[i]));
+            answer[i] = answer[i].replace("1","#");
+            answer[i] = answer[i].replace("0"," ");  
+            System.out.println(answer[i]);
+        }
+        return answer;
+    }
+}
